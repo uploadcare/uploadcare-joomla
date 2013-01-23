d53601b370fcae6bec2735440421c39ea1d850f4
Gray Hound <admin@devilmaydie.name>
2013-01-23 16:56:41 +0400

    Bugfix

--- component/admin/views/uploadcare/view.html.php
+++ component/admin/views/uploadcare/view.html.php
@@ -2,21 +2,22 @@
 defined('_JEXEC') or die('Restricted access');
 jimport('joomla.application.component.view');
 class UploadcareViewUploadcare extends JView
-{ 
-		public function display($tpl = null) 
-		{
-			$this->addToolBar();
-			$this->setDocument();
-		}
-        protected function addToolBar() 
-        {
-                JToolBarHelper::title('Uploadcare');
-                JToolBarHelper::preferences('com_uploadcare');
-        }
-		
-		function setDocument() 
-        {
-                $document = JFactory::getDocument();
-                $document->setTitle('Uploadcare Administration');
-        }
+{
+	public function display($tpl = null)
+	{
+		$this->addToolBar();
+		$this->setDocument();
+		parent::display($tpl);
+	}
+	protected function addToolBar()
+	{
+		JToolBarHelper::title('Uploadcare');
+		JToolBarHelper::preferences('com_uploadcare');
+	}
+
+	function setDocument()
+	{
+		$document = JFactory::getDocument();
+		$document->setTitle('Uploadcare Administration');
+	}
 }
